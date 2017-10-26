3301815c88a94519041f8fa902ad5f77e5555be2
Alex Mellnik <a.r.mellnik@gmail.com>
2017-10-25 21:42:43 -0700

    Call functions once to reduce startup time

--- setup.jl
+++ setup.jl
@@ -10,3 +10,21 @@ for p in packages
     eval(parse(string("using ", p)))
 end
 using ExportWebAssembly
+
+# Make a single call to warm up
+using StaticArrays
+struct X
+    a::Float64
+    b::Float64
+end
+
+function unlikely_named_fn(a)
+    x = SVector(1., 2., a)
+    y = SVector(a, a, 3.)
+    z = X(a, 2a)
+    return sum(x + 2y) - z.b + z.a
+end
+
+export_bitcode("unlikely_named_fn.bc", unlikely_named_fn, [Float32])
+
+# Also call emscripten once
