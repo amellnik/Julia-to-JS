266948a31d4205f1768c1fff6d13997e474088b0
Alex Mellnik <a.r.mellnik@gmail.com>
2018-04-08 16:34:17 -0700

    Expand documentation

--- README.md
+++ README.md
@@ -1,19 +1,13 @@
 # What's this?
 
-This repository is a harebrained attempt to create a web application that takes Julia code and converts it to Javascript via Tom Short's [ExportWebAssembly.jl](https://github.com/tshort/ExportWebAssembly.jl) and [Emscripten](http://kripken.github.io/emscripten-site/index.html).  You can try it [here](https://alex.mellnik.net/julia-2-js/).    
+This repository is a harebrained attempt to create a web application that takes Julia code and converts it to something that can run on the web via either
+* Tom Short's [ExportWebAssembly.jl](https://github.com/tshort/ExportWebAssembly.jl), [CodeGen.jl](https://github.com/tshort/CodeGen.jl) and [Emscripten](http://kripken.github.io/emscripten-site/index.html).  
+* Mike Innes's [Charlotte.jl](https://github.com/MikeInnes/Charlotte.jl) and the [WebAssembly Binary Toolkit](https://github.com/WebAssembly/wabt).
 
-## Here's the plan
+After each tool converts your julia function to either javascript or wasm it loads the result into the browser window so that you can test it out.  This is all very experimental, so don't be surprised when things don't work.
 
-The front-end presents the user with a large textarea input (it would be great if it had syntax highlighting) for a julia script.  The user pastes in something like
+You can try it [here](http://julia2js.gotfork.net/).    
 
-```julia
-function my_fun(x)
-    return (x < 2) ? 1 : my_fun(x-1) + my_fun(x-2)
-end
-```
+## One more thing
 
-and clicks submit.  This posts the contents of the function to the back-end, which saves it in the container's temporary filesystem.  This file is converted to `.bc` and then to `.js` (eventually `.wasm`), before being loaded into the starting webpage.  At that point, you can try out the function to see if it worked (or read a long cryptic error message).
-
-## This is very experimental
-
-Many common operations and functions won't work, but keep an eye on [ExportWebAssembly.jl](https://github.com/tshort/ExportWebAssembly.jl) and [CodeGen.jl](https://github.com/tshort/CodeGen.jl) for updates.  
+The API for the Charlotte.jl conversion is written in [HTTP.jl](https://github.com/JuliaWeb/HTTP.jl) with a bit of help from [Joseki.jl](https://github.com/amellnik/Joseki.jl), and is intended to serve as an example API written entirely in Julia.  
--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Http } from '@angular/http';
-import { ApiService } from './api.service';
 import { ClarityIcons } from 'clarity-icons';
 import 'codemirror/mode/julia/julia';
 
@@ -8,14 +6,11 @@ import 'codemirror/mode/julia/julia';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [ApiService]
+  providers: []
 })
 export class AppComponent implements OnInit {
 
-  constructor (
-    private http: Http,
-    private ApiService: ApiService
-  ) {
+  constructor () {
     ClarityIcons.add({"julia": `<svg
      version="1.1"
      viewBox="0 0 104.33789 96.837892"
@@ -50,119 +45,7 @@ export class AppComponent implements OnInit {
 
 
 
-  cmConfig = {
-    mode: 'julia',
-    indentWithTabs: true,
-    lineNumbers: true
-    // theme: 'neat',
-    // addModeClass: true
-  };
-
-  jlCode = "";
-  jlFunction = "";
-  jlTypes = "";
-  serverResponse:any;
-  jsCode = "";
-  serverError = "";
-  jsFunctionCall = "";
-  jsResult = "\n";
-  waiting_on_API = false;
-  filename = "";
-
-
   ngOnInit() {
-    this.loadDemo();
   }
 
-  loadDemo() {
-    this.jlCode = `function fib(n)
-    n < 2 ? 1 : fib(n-1) + fib(n-2)
-end`;
-    this.jlFunction = "fib";
-    this.jlTypes = "Int32";
-    this.jsFunctionCall="_fib(6)"
-  }
-
-  sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
-  submit() {
-    // Get rid of any existing code including the injected portion
-    this.jsCode = "";
-    this.serverError = "";
-    if (document.contains(document.getElementById("injected-js"))) {
-      console.log("Removing old js code");
-      document.getElementById("injected-js").remove();
-      // Also delete the global variables for it
-      window['Module'] = {};
-    }
-
-    this.waiting_on_API = true;
-    return this.ApiService.submitForJS(this.jlCode.replace(/\n/g, "\n"), this.jlFunction, this.jlTypes).subscribe(
-        data => this.filename = data.filename,
-        error => this.handleServerError(error),
-        () => this.runChecks(30)
-      );
-  }
-
-  runChecks(retries:number) {
-    console.log("Checking server, retries left: " + retries)
-    if (retries == 0 ) {
-      console.log("Ran out of retries -- something went terribly wrong...");
-      this.waiting_on_API = false;
-      this.serverError = "The server never created a response for this request -- something must have gone terribly wrong."
-      return 0;
-    }
-    this.ApiService.checkForResult(this.filename).subscribe(
-        data => this.serverResponse = data,
-        error => this.handleServerError(error),
-        () => {
-          if (this.serverResponse.ready) {
-            this.afterQuery();
-          } else {
-            this.sleep(5000).then(() => {
-              this.runChecks(retries-1);
-            })
-          }
-        }
-      );
-  }
-
-
-
-  afterQuery() {
-    this.waiting_on_API = false;
-    // Check response to see if there was a conversion error
-    if (this.serverResponse.hasOwnProperty('error')) {
-      this.serverError = this.serverResponse.error;
-      console.log(this.handleServerError);
-      // Show server error here
-    } else {
-      this.jsCode = this.serverResponse.data;
-      this.addJsToElement(this.jsCode);
-      console.log("Loaded some sketchy js!")
-    }
-  }
-
-  handleServerError(error: any) {
-    console.log(error);
-    this.waiting_on_API = false;
-    this.serverError = `Unexciting server error.  If it's timeout related, try again in ~30 sec.
-
-    ` + error._body.toString();
-  }
-
-  run_js() {  // No clue if this sort of thing works
-    this.jsResult = '' + eval(this.jsFunctionCall);
-  }
-
-  addJsToElement(src: string): HTMLScriptElement {
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.innerHTML = src;
-    script.id = "injected-js";
-    document.getElementsByTagName('head')[0].appendChild(script);
-    return script;
-  }
 }
